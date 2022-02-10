a31df3c3183dd4cf6855752def10f1e79952e658
Rood17 <kochehua@gmail.com>
2022-02-10 09:19:20 -0600

    API getUF ready!

--- JRapp/app/utils/services/get_services.js
+++ JRapp/app/utils/services/get_services.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { SECRET_AUTH, AUTHORIZATION, GET, POST } from '../../../types'
+import { SECRET_AUTH, AUTHORIZATION, GET, POST,
+  URL_OUTH, BASIC, CONTENT_TYPE, TYPE_URLENCODED, BEARER,
+    } from '../../../types'
 import * as data from '../../utils/services/perfil_uf.json'
 import * as paquetes from '../../utils/services/paquetes.json'
 import * as qs from 'qs'
@@ -14,29 +16,35 @@ export const getDataJson = () => {
   return data
 }
 
-export const getPerfilUf = async (idSubscriber) => {
-  const object = {};
-  const secret = getToken()
+export const getPerfilUf = (idSubscriber) => {
+  return getTokenAndUF(idSubscriber)
+}
 
-  print(" pachihihihi : "+ secret.accessToken)
+export const getPerfilUfAPI = async (idSubscriber, secret) => {
+  const object = {};
 
 
   const result = { statusResponse: true, error: null, userData: [] };
   let axios = require('axios');
 
+  
+  if ( idSubscriber == 5688888888 || idSubscriber == 8888888888)
+    idSubscriber = 1000223908;
+
   let config = {
     method: GET,
-    url: `https://altanredes-prod.apigee.net/cm-sandbox/v1/subscribers/${idSubscriber}/profile`,
+    url: `https://altanredes-prod.apigee.net/cm/v1/subscribers/${idSubscriber}/profile`,
     headers: {
-      AUTHORIZATION: getToken().accessToken
+      AUTHORIZATION: secret
     }
   };
   try {
     await axios(config)
       .then(function (response) {
         //console.log(JSON.stringify(response.data));
-        result.userData = '[' + JSON.stringify(response.data) + ']';
-        console.log("Services response.data : " + result.userData)
+        result.userData = response.data.responseSubscriber.information;
+        console.log("Services response.data : " + result.userData.idSubscriber)
+        //console.log("Services response.data : " + response.data[responseSubscriber])
       })
       .catch(function (error) {
         console.log(error);
@@ -55,30 +63,35 @@ export const getPerfilUf = async (idSubscriber) => {
   return result;
 };
 
-export const getToken = async (idSubscriber) => {
+export const getTokenAndUF = async (idSubscriber) => {
   const axios = require('axios');
   const qs = require('qs');
+  
   let result;
   let data = qs.stringify({
 
   });
   let config = {
-    method: 'post',
-    url: 'https://altanredes-prod.apigee.net/v1/oauth/accesstoken?grant-type=client_credentials',
+    method: POST,
+    url: URL_OUTH,
     headers: {
-      'Authorization': 'Basic MVVlYWRvVmFMWm9Fb2dORzZBMHVmN1lkeXpwemM2R0g6Nm9GMUMxd0sxMEdXS3dJUw==',
-      'Content-Type': 'application/x-www-form-urlencoded'
+      AUTHORIZATION: BASIC,
+      CONTENT_TYPE: TYPE_URLENCODED
     },
     data: data
   };
 
-  axios(config)
-    .then((response) => {
-      result = response
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  let myPromise = new Promise(function (resolve) {
+    axios(config)
+      .then((response) => {
+        const secret = BEARER + response.data.accessToken
+        resolve(getPerfilUfAPI(idSubscriber, secret))
+      })
+      .catch((error) => {
+        console.log(error);
+      }).finally(() => console.log("Perfil UF Listo"));
+  });
 
+  result = await myPromise
   return result;
 };
